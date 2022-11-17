882d88c94fd582508863ddbe5bb8f5249d3e5ba1
guaaan <juampablo.txt@gmail.com>
2022-11-17 19:03:35 -0300

    elementos de inventario agregados en durp

--- Assets/Scripts/InventarioManager.cs
+++ Assets/Scripts/InventarioManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class InventarioManager : MonoBehaviour
 {
+    //construyo la struct objeto del inventario
     [System.Serializable]
     public struct ObjetoInventarioId
     {
@@ -25,20 +26,24 @@ public class InventarioManager : MonoBehaviour
 
 
     public InventarioBaseDatos baseDatos;
-
     public List<ObjetoInventarioId> inventario;
 
+    //agrego al inventario
     public void AgregarAlgoAlInventario(int id, int cantidad)
     {
         for (int i = 0; i < inventario.Count; i++)
         {
+            // si existe en la lista no lo agrego sino que sumo
             if (inventario[i].id == id)
             {
                 inventario[i].sumarCantidad(cantidad);
+                ActualizarInventario();
                 return;
             }
         }
+        // si no existe en la lista lo agrego nuevo
         inventario.Add(new ObjetoInventarioId(id, cantidad));
+        ActualizarInventario();
     }
 
     public void EliminarAlgoDeInventario(int id, int cantidad)
@@ -47,12 +52,60 @@ public class InventarioManager : MonoBehaviour
         {
             if (inventario[i].id == id)
             {
+                // si hay más de uno le resto la cantidad
                 inventario[i].sumarCantidad(-cantidad);
+                // si queda en cero elimino el item
                 if (inventario[i].cantidad <= 0)
                     inventario.Remove(inventario[i]);
+                ActualizarInventario();
                 return;
             }
         }
         Debug.LogError("No exite el objeot a eliminar");
     }
+
+    public void Start()
+    {
+        ActualizarInventario();
+    }
+
+    public InventarioObjetoInterface prefab;
+    public Transform inventarioUI;
+    // pool es los objetos que no sabemos si vamos a utilizar así que lo desactivamos
+    List<InventarioObjetoInterface> pool = new List<InventarioObjetoInterface>();
+
+    public void ActualizarInventario()
+    {
+        for (int i = 0; i < pool.Count; i++)
+        {
+            if (i < inventario.Count)
+            {
+                ObjetoInventarioId o = inventario[i];
+                pool[i].sprite.sprite = baseDatos.baseDatos[o.id].sprite;
+                pool[i].cantidad.text = o.cantidad.ToString();
+                pool[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                pool[i].gameObject.SetActive(false);
+            }
+        }
+        {
+            if (inventario.Count > pool.Count)
+                for (int i = pool.Count; i < inventario.Count; i++)
+                {
+                    InventarioObjetoInterface oi = Instantiate(prefab, inventarioUI);
+                    pool.Add(oi);
+                    // escala y posiciona automaticamente en el canvas
+                    oi.transform.position = Vector3.zero;
+                    oi.transform.localScale = Vector3.one;
+
+                    ObjetoInventarioId o = inventario[i];
+                    pool[i].sprite.sprite = baseDatos.baseDatos[o.id].sprite;
+                    pool[i].cantidad.text = o.cantidad.ToString();
+                    pool[i].gameObject.SetActive(true);
+                }
+        }
+    }
+
 }
---
+++ Assets/Scripts/InventarioObjetoInterface.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventarioObjetoInterface : MonoBehaviour
+{
+    public TMP_Text cantidad;
+    public Image sprite;
+}
