b6ee8c490a6161e8766ce7ef90403b4165c57e19
guaaan <juampablo.txt@gmail.com>
2022-11-18 15:13:44 -0300

    se pueden arrastrar objetos de inventario pero hasta ahi nomas

--- Assets/Scripts/InventarioObjetoInterface.cs
+++ Assets/Scripts/InventarioObjetoInterface.cs
@@ -2,11 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InventarioObjetoInterface : MonoBehaviour
+public class InventarioObjetoInterface : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public TMP_Text cantidad;
     public Image sprite;
     public Button boton;
+
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // le pongo el sprite al placeholder
+        InventarioObjetoInterfacePlaceholder.current.sprite.sprite = sprite.sprite;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        InventarioObjetoInterfacePlaceholder.current.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
 }
